7acd550e8e873b6cd9d492b2060bc6c8033dc3f5
Amauri Rossetti Giovani <amaurirg@terra.com.br>
2022-10-20 08:11:32 -0300

    some examples

---
+++ requests-http/src/app/cursos/cursos.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Curso } from './curso';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CursosService {
+
+  private readonly API = 'http://localhost:3000/cursos';
+
+  constructor(private http: HttpClient) { }
+  
+  lista() {
+    return this.http.get<Curso[]>(this.API);
+  }
+}
